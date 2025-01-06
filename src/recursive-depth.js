7bf89bf5f9d285d59f18d686514356b63b1a83d2
Tanja Kiseleva <lessy_92@mail.ru>
2025-01-06 15:56:40 +0100

    fix: getCommonCharacterCount,

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,9 +13,27 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  calculateDepth(arr) {
+    if (!Array.isArray(arr)) {
+      return 0;
+    }
+
+    if (arr.length === 0) {
+      return 1; 
+    }
+
+    let maxDepth = 0;
+
+    for (let element of arr) {
+      if (Array.isArray(element)) {
+        const currentDepth = this.calculateDepth(element);
+        if (currentDepth > maxDepth) {
+          maxDepth = currentDepth;
+        }
+      }
+    }
+
+    return maxDepth + 1;
   }
 }
 
